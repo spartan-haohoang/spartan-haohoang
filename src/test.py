c1095670fc9ecb00ee300ff6a2729240943c42d2
Hao Hoang <hao.hoang@c0x12c.com>
2025-03-27 08:34:06 +0700

    add some importance distance

--- src/test.py
+++ src/test.py
@@ -1,38 +1,44 @@
 import numpy as np
 
 
-def divide(a, b):
-    return a / b
-
-
-def sum(a, b):
-    return a + b
-
-
-def sum_of_squares(a, b):
-    return a**2 + b**2
-
-
-def mahalanobis_distance(x, y):
-    return np.sqrt(np.sum((x - y) ** 2))
-
-
+# List out some distance metrics for calculating the distance between two vectors
+## 1. Euclidean Distance
 def euclidean_distance(x, y):
     return np.sqrt(np.sum((x - y) ** 2))
 
 
+## 2. Manhattan Distance
 def manhattan_distance(x, y):
     return np.sum(np.abs(x - y))
 
 
-print(mahalanobis_distance(np.array([1, 2, 3]), np.array([4, 5, 6])))
-print(euclidean_distance(np.array([1, 2, 3]), np.array([4, 5, 6])))
-print(manhattan_distance(np.array([1, 2, 3]), np.array([4, 5, 6])))
+## 3. Minkowski Distance
+def minkowski_distance(x, y, p=2):
+    return np.sum(np.abs(x - y) ** p) ** (1 / p)
 
-print(sum_of_squares(1, 2))
 
-print(sum(1, 2))
+## 4. Cosine Distance
+def cosine_distance(x, y):
+    return np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
 
-print(divide(1, 2))
 
-print(divide(1, 0))
+## 5. Jaccard Distance
+def jaccard_distance(x, y):
+    return np.sum(np.abs(x - y)) / np.sum(np.abs(x + y))
+
+
+## 6. Hamming Distance
+def hamming_distance(x, y):
+    return np.sum(x != y)
+
+
+# Test the distance metrics
+x = np.array([1, 2, 3])
+y = np.array([4, 5, 6])
+
+print(euclidean_distance(x, y))
+print(manhattan_distance(x, y))
+print(minkowski_distance(x, y))
+print(cosine_distance(x, y))
+print(jaccard_distance(x, y))
+print(hamming_distance(x, y))
